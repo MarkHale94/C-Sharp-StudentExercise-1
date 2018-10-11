0b5bba28944f9b488f7f9a974b709fe61d2b7ea3
Mark Hale <mark.hale94@gmail.com>
2018-10-11 09:27:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise1CustomTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
